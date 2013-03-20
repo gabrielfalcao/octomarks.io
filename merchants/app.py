a01eb5bbbc16315877b442b7b572aa28e2defb75
Lincoln de Sousa <lincoln@comum.org>
2013-03-20 13:43:59 -0400

    First steps for the main dashboard page

--- merchants/app.py
+++ merchants/app.py
@@ -13,6 +13,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/dashboard/competitors')
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard/main.html')
+
+
+@app.route('/dashboard/competitors/')
 def dashboard_competitors():
     return render_template('dashboard/competitors.html')

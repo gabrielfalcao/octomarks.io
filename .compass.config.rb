ff2acd39e8d900430838b84e8f54e03460dded42
Fabio M. Costa <fabiomcosta@gmail.com>
2013-03-20 16:27:42 -0400

    adding the compass config file

---
+++ .compass.config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/css"
+sass_dir = "static/sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+generated_images_dir = "static/"
+additional_import_paths = [
+  "modules/yipit-static-assets/yipit_static_assets/static"
+]
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :nested # :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false

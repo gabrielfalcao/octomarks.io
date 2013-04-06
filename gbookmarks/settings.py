23941348a2b2e0862abd080d47460f6a2644ff43
Gabriel Falcao <gabriel@nacaolivre.org>
2013-04-06 00:38:53 -0400

    db and requirements

--- gbookmarks/settings.py
+++ gbookmarks/settings.py
@@ -13,7 +13,10 @@ PRODUCTION = not DEBUG
 SECRET_KEY = 'P4wned!'
 
 # Database connection URI
-DATABASE = 'mysql://root@localhost/gb'
+DATABASE = 'mysql://gbookmarks:b00k@BABY@mysql.gabrielfalcao.com/gbookmarks'
+
+if not PRODUCTION:
+    DATABASE = 'mysql://root@localhost/gb'
 
 # Static assets craziness
 LOCAL_FILE = lambda *p: abspath(join(__file__, '..', *p))

96c845ff06d4ef940ffc096ce6ab5ed479c4c62d
Gabriel Falcao <gabriel@nacaolivre.org>
2013-04-09 15:48:59 -0400

    no more production database in local

--- gbookmarks/settings.py
+++ gbookmarks/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = 'P4wned!'
 # Database connection URI
 DATABASE = 'mysql://gbookmarks:b00k@BABY@mysql.gabrielfalcao.com/gbookmarks'
 
-# if not PRODUCTION:
-#     DATABASE = 'mysql://root@localhost/gb'
+if not PRODUCTION:
+    DATABASE = 'mysql://root@localhost/gb'
 
 # Static assets craziness
 LOCAL_FILE = lambda *p: abspath(join(__file__, '..', *p))

b8115196c7c865e8101d197a2f6745adddc4c29b
Fabio M. Costa <fabiomcosta@gmail.com>
2013-04-12 18:18:08 -0300

    using an image to create the get request.

--- gbookmarks/templates/bookmarklet.js
+++ gbookmarks/templates/bookmarklet.js
@@ -1,16 +1,13 @@
 (function(){
-    if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
-        script = document.createElement( 'script' );
-        script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-        script.onload=save_bookmark;
-        document.body.appendChild(script);
-    }
-    else {
-        save_bookmark();
-    }
 
-    function save_bookmark() {
-        var URL = encodeURIComponent(location.href);
-        location.href='{{ full_url_for(".save_bookmark", token=user.gb_token) }}?uri='+encodeURIComponent(location.href)+'&should_redirect=1&time='+(new Date().getTime());
-    }
-})()
+    var URL = encodeURIComponent(location.href);
+    var image = document.createElement('img');
+    image.onload = function() {
+        console.log('nice!!');
+    };
+    image.onerror = function() {
+        console.log('error!');
+    };
+    image.src = '{{ full_url_for(".save_bookmark", token=user.gb_token) }}?uri='+encodeURIComponent(location.href)+'&should_redirect=1&time='+(new Date().getTime());
+    
+})();

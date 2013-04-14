8c54faeacfeb3d02d2b13f39912d29874c4c4fb8
Gabriel Falcao <gabriel@nacaolivre.org>
2013-04-14 00:28:21 -0400

    adding unique constraint for url + token

---
+++ gbookmarks/migrations/versions/b695424e9e8_add_unique_constrain.py
@@ -0,0 +1,25 @@
+"""add unique constraint for url + token
+
+Revision ID: b695424e9e8
+Revises: 273eff505736
+Create Date: 2013-04-13 23:54:39.619365
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b695424e9e8'
+down_revision = '273eff505736'
+
+from alembic import op
+
+
+def upgrade():
+    op.drop_constraint('url', 'gb_http_cache', 'unique')
+    op.drop_constraint('token', 'gb_http_cache', 'unique')
+    op.create_unique_constraint('url_token', 'gb_http_cache', ["url", "token"])
+
+
+def downgrade():
+    op.drop_constraint('url_token', 'gb_http_cache', 'unique')
+    op.create_unique_constraint("url", "gb_http_cache", ["url"])
+    op.create_unique_constraint("token", "gb_http_cache", ["token"])
